8981d1ffeaea5a8d3fb90093b2532b78a9913616
AARTHI B <aarthib875@gmail.com>
2025-12-02 20:51:03 +0530

    Update index.js

--- ExpenseTracking/Api/index.js
+++ ExpenseTracking/Api/index.js
@@ -18,6 +18,17 @@ app.use(
     credentials: true
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL); // frontend URL
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // respond to preflight immediately
+  }
+  next();
+});
 
 app.use(express.urlencoded({extended:false}))
 
